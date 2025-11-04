ed55abbb45cab863f2d96b1201a6d8d255095d88
GabyGaby412 <g.velazquez2@ufl.edu>
2025-11-04 14:06:25 -0500

    added a few things

--- main.cpp
+++ main.cpp
@@ -98,7 +98,4 @@ struct Tweet {
 
    vector<int> data3 = tweets.tweet_length;
 }
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+//check now
