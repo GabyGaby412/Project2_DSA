4e598f0608cf46663b81732960c91bdb9427fbff
Carven-Louis <carvenlouis05@gmail.com>
2025-11-06 11:44:50 -0500

    Fixed quicksort, was mixing include <algorithm>

--- Quicksort.h
+++ Quicksort.h
@@ -3,6 +3,7 @@
 #include <utility>
 #include <vector>
 #include "node.h"
+#include <algorithm>
 using namespace std;
 #pragma once
 
@@ -28,10 +29,10 @@ int pivot(std::vector<Node> date, int low, int high) {
         }
 
         if (up < down) {
-            swap(date[up].get_date(), date[down].get_date());
+            std::swap(date[up], date[down]);
         }
     }
-    swap(date[low].get_date(), date[down].get_date());
+    std::swap(date[low], date[down]);
     return down;
 }
 
@@ -46,44 +47,44 @@ void quickSort(vector<Node> &date, int low, int high) {
 
     }
 }
-
-//tried doing this with len but there is something strange going on
-int pivot_len(std::vector<Node> tokens, int low, int high) {
-    int pivot = tokens[low].get_tweet_len();
-    int up = low;
-    int down = high;
-
-    while (up < down) {
-        for (int i = up; i < high; i++) {
-            if (tokens[up].get_tweet_len() > pivot) {
-                break;
-            }
-            up++;
-        }
-
-        for (int j = high; j > low; j--) {
-            if (tokens[down].get_tweet_len() < pivot) {
-                break;
-            }
-            down--;
-        }
-
-        if (up < down) {
-            swap(&data[up].get_tweet_len(), &data[down].get_tweet_len());
-        }
-    }
-    swap(data[low].get_tweet_len(), &data[down].get_tweet_len());
-    return down;
-}
-
-
-//The quicksort
-
-void quickSort_len(vector<Node> &tokens, int low, int high) {
-    if (low < high) {
-        int middle = pivot(tokens, low, high);
-        quickSort(tokens, low, middle -1);
-        quickSort(tokens, middle +1, high);
-
-    }
-}
+//
+// //tried doing this with len but there is something strange going on
+// int pivot_len(std::vector<Node> tokens, int low, int high) {
+//     int pivot = tokens[low].get_tweet_len();
+//     int up = low;
+//     int down = high;
+//
+//     while (up < down) {
+//         for (int i = up; i < high; i++) {
+//             if (tokens[up].get_tweet_len() > pivot) {
+//                 break;
+//             }
+//             up++;
+//         }
+//
+//         for (int j = high; j > low; j--) {
+//             if (tokens[down].get_tweet_len() < pivot) {
+//                 break;
+//             }
+//             down--;
+//         }
+//
+//         if (up < down) {
+//             swap(&data[up].get_tweet_len(), &data[down].get_tweet_len());
+//         }
+//     }
+//     swap(data[low].get_tweet_len(), &data[down].get_tweet_len());
+//     return down;
+// }
+//
+//
+// //The quicksort
+//
+// void quickSort_len(vector<Node> &tokens, int low, int high) {
+//     if (low < high) {
+//         int middle = pivot(tokens, low, high);
+//         quickSort(tokens, low, middle -1);
+//         quickSort(tokens, middle +1, high);
+//
+//     }
+// }
