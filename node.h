ff2fe67bdc9ea2fca185c1e0f2d38c4652d9da16
Beau117 <beaubrown141@yahoo.com>
2025-11-05 20:09:30 -0500

    Update node.h
    separated into two files and fixed some errors

--- node.h
+++ node.h
@@ -1,53 +1,19 @@
 #include <vector>
+#include <string>
 #pragma once
 
 class Node {
     int date;
+    int time;
     std::string username;
     std::string tweet;
     int tweet_len;
 public:
-    Node(int month, int day, int year, std::string username, std::string tweet);
-    ~Node();
     Node();
+    Node(int month, int day, int year, std::string username, std::string tweet, std::string time_s);
     int get_date();
     std::string get_tweet();
     std::string get_username();
     int get_tweet_len();
+    int get_time();
 };
-Node::Node() {
-    this->date = 0;
-    this->username = "";
-    this->tweet = "";
-    this->tweet_len = 0;
-}
-Node::Node(int month, int day, int year, std::string username, std::string tweet) {
-    int temp = month * 12 + day;
-    int temp_year = year - 2000;
-    int leap_year = 0;
-    for (int i = 0; i < temp_year; i += 4) {
-        leap_year++;
-    }
-    temp += leap_yer;
-    temp_year *= 365;
-    temp += temp_year;
-    this->date = temp;
-    this->username = username;
-    this->tweet = tweet;
-    this->tweet_len = tweet.size();
-}
-Node::~Node() {
-    delete Node;
-}
-int Node::get_date() {
-    return this->date;
-}
-std::string Node::get_tweet() {
-    return this->tweet;
-}
-std::string Node::get_username() {
-    return this->username;
-}
-int Node::get_tweet_len() {
-    return this->tweet_len;
-}
