652b4dbe56961dbcd3e019d6d899cc93daa1d4b8
GabyGaby412 <g.velazquez2@ufl.edu>
2025-11-08 20:59:02 -0500

    Update dataset.py to full dataset
    Refactor dataset processing to save tokenized words and count tokens.

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ right now.
 This code was also refurbished from my computaional linguistics class. '''
 import pandas as pd
 import re
+import csv
 import ast
 
 df = pd.read_csv('training.1600000.processed.noemoticon.csv', 
@@ -20,19 +21,15 @@ df = pd.read_csv('training.1600000.processed.noemoticon.csv',
 df["tokens"] = [re.findall(r"\b\w+\b", str(t).lower()) for t in df["text"]]
 
 # df.to_csv("tokenized_dataset.csv", index=False)
-# print("✅ Tokenized words saved to tokenized_dataset.csv")
+# print("Tokenized words saved to tokenized_dataset.csv")
 
-df = pd.read_csv("cmake-build-debug/tokenized_dataset.csv")
+df["tokens"] = df["tokens"].apply(len)
 
-df["tokens"] = df["tokens"].apply((ast.literal_eval))
-df["token_count"] = df["tokens"].apply(len)
+df.to_csv("tokenized_dataset.csv", index=False, quoting=csv.QUOTE_MINIMAL)
 
 tweet_text = df["text"]
 df["tweet_length"] = tweet_text.apply(len)
 
-# print(tweet_length)
-# tweet_lengths = df['length'].tolist()
-# print(tweet_lengths[10])
 
 
 
