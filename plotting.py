9cd640b2d70f26d755fb1ada751e80175f1c728c
Carven-Louis <carvenlouis@ufl.edu>
2025-11-08 19:46:39 -0500

    Slight Change

--- plotting.py
+++ plotting.py
@@ -10,7 +10,7 @@ fig, axes = plt.subplots(2, 1, figsize=(12, 7))
 data = pd.read_csv('tweet_lengths.csv')
 #plt.figure(figsize=(12,6))
 axes[0].bar(data["tweet"], data[" length"])
-axes[0].set_title("Random 15 Tweet Lengths after sorted")
+axes[0].set_title("Random Tweet Lengths After Sorted")
 
 
 data2 = pd.read_csv("sorting_time.csv")
@@ -18,7 +18,7 @@ data2[" time"] = data2[" time"].str.replace("ms", "", regex = False).astype(floa
 
 axes[1].set_position([0.15, 0.1, 0.3, 0.35])
 axes[1].bar(data2["sorting"], data2[" time"], width=0.6)
-axes[1].set_title("Sorting Time Comparisons in milliseconds")
+axes[1].set_title("Sorting Time Comparisons in Milliseconds")
 axes[1].set_ylim(bottom=0)
 
 plt.show()
